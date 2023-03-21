d9010b3e7e0fa4d8ab49520ff3392f5a4aaf6b11
Pedro Lessa <pepeulessa@hotmail.com>
2023-03-21 19:25:07 -0300

    Finalização CSS e HTML básicos, início Script

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-let botao = document.querySelector('.dark-mode-input')
-botao.addEventListener('click', ()=>{
+let botaoDark = document.querySelector('.dark-mode-input')
+botaoDark.addEventListener('click', ()=>{
     console.log('Clicou')
 })
+
+async function buscaPaises(){
+    let p = await fetch('https://restcountries.com/v3.1/all')
+    let paises = await p.json()
+    console.log(paises[0])
+}
+buscaPaises()
